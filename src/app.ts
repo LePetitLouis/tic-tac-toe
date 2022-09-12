69f0f453eb6026fc2237d550c0c660169e4dddf3
LePetitLouis <44061639+LePetitLouis@users.noreply.github.com>
2022-09-12 11:49:40 +0200

    wip

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-import * as cors from "cors";
+var cors = require('cors')
+// import * as cors from "cors";
 import "reflect-metadata";
 
 var indexRouter = require("./routes/index");
